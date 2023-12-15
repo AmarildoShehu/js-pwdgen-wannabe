4e2dc3f8f182a295db39b6fafecf7136a662e53b
Amarildo <amarildos@outloo.it>
2023-12-15 16:15:24 +0100

    surname request added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ console. log('JS OK');
 // 2. chiedo all'utente il suo nome.
 let name= prompt('Inserisci il tuo nome');
 console.log(name);
+
+// 3. chiedo all'utente il suo cognome.
+let surname= prompt('Inserisci il tuo cognome');
+console.log(surname);
