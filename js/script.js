5d958fffb69897cdda55d74c1c4c8be1f03079e5
Amarildo <amarildos@outloo.it>
2023-12-15 16:35:20 +0100

    previous data gruped

--- js/script.js
+++ js/script.js
@@ -2,15 +2,20 @@
 console. log('JS OK');
 
 // 2. chiedo all'utente il suo nome.
-let name= prompt('Inserisci il tuo nome');
+let name = prompt('Inserisci il tuo nome');
 console.log(name);
 
 // 3. chiedo all'utente il suo cognome.
-let surname= prompt('Inserisci il tuo cognome');
+let surname = prompt('Inserisci il tuo cognome');
 console.log(surname);
 
 // 4. chiedo all'utente il suo colore preferito.
-let color= prompt('Inserisci il tuo colore preferito');
+let color = prompt('Inserisci il tuo colore preferito');
 console. log(color);
 
 // 5. inserisco nell'html un id per mostrare a schermo la password.
+
+// 6. concateno i valori per creare la password.
+let password = name+surname+color;
+console.log(password);
+
