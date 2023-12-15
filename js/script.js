9d1ba9f2558d52c8e785be1741b1fe82e570e64c
Amarildo <amarildos@outloo.it>
2023-12-15 17:11:23 +0100

    changed variable to const

--- js/script.js
+++ js/script.js
@@ -2,22 +2,22 @@
 console. log('JS OK');
 
 // 2. chiedo all'utente il suo nome.
-let name = prompt('Inserisci il tuo nome');
+const name = prompt('Inserisci il tuo nome');
 console.log(name);
 
 // 3. chiedo all'utente il suo cognome.
-let surname = prompt('Inserisci il tuo cognome');
+const surname = prompt('Inserisci il tuo cognome');
 console.log(surname);
 
 // 4. chiedo all'utente il suo colore preferito.
-let color = prompt('Inserisci il tuo colore preferito');
+const color = prompt('Inserisci il tuo colore preferito');
 console. log(color);
 
 // 5. inserisco nell'html un id per mostrare a schermo la password.
 
 // 6. concateno i valori per creare la password.
 // 7. inserisco il numero '21' da agganciare alla password.
-let password = name+surname+color+21;
+const password = name+surname+color+21;
 console.log(password);
 
 // 8. Mostra la password in pagina.
